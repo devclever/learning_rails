dc8893141570803c98b581fc71dcbf10258ffe25
Dmitry Golubev <devclever@gmail.com>
2013-05-03 10:16:14 +0600

    Add micropost resource and add content validateion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503035037) do
+ActiveRecord::Schema.define(:version => 20130503040645) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "uset_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
